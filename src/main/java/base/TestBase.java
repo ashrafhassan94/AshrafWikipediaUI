e21b901a4dd0eb678b4d3ea4e6d7947ed8bb704b
Ashraf Ismail <ashraf.ismail@softxpert.com>
2024-03-16 13:51:23 +0200

    resolving conflict with getdriver

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -1,21 +1,30 @@
 package base;
 import configurations.Config;
+import configurations.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import org.testng.annotations.*;
 import org.openqa.selenium.WebDriver;
 
 import static base.PageBase.driver;
+import static configurations.Constants.WikiURL;
 
 public class TestBase implements _DriverManager {
 
     public static Config config = new Config();
 
-
+    public static WebDriver driver;
+    @BeforeSuite
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(WikiURL);
+    }
     public static WebDriver getDriver() {
-        return driver.get("https://en.wikipedia.org/wiki/Main_Page");
+       return driver;
     }
 
 }
---
+++ src/main/java/configurations/Constants.java
@@ -0,0 +1,6 @@
+package configurations;
+
+public class Constants {
+
+    public static final String WikiURL = "https://en.wikipedia.org/wiki/Main_Page";
+}
